ecc3110ec553525c9e69276f73fb4f7bf2c0b350
Sanyiah Zulfiqar <sanyiahzulfiqar1996@gmail.com>
2022-05-19 10:26:46 +0100

    edited function

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ switch(answers.option){
  viewAllRoles()
  break;
 
- case 'view all employees':
+ case 'View all employees':
   viewAllEmployees()
   break;
 
@@ -47,7 +47,7 @@ default:
   db.end()
 }
 });
-
+} 
 mainMenu()
 
 
@@ -101,40 +101,115 @@ function viewAllRoles (){
      
         }
       ]).then(function(answer){
-        Connection.query(
-          "INSERT INTO department(name)",
-          [answer.department],
+        db.query(
+          "INSERT INTO department (name) VALUES (?)",
+          [answer],
           function(err, results, fields) {
-          console.log(New department added); 
+          console.log("New department added"); 
           
               mainMenu();
             }
             )
-         });
+         })};
 
-         
-  
 
-  
-      function addARoll() {
-        db.query(
-          'SELECT * FROM `role`',
-          function(err, results, fields) {
-            console.log(results); 
-            mainMenu();
-          }
-        )};
     
+
+          async function addARoll(departmentID, departmentName) {
+              db.promise().query(
+                'SELECT * FROM `department`'
+              ).then (([departments])=>{
+                departmentChoices= departments.map ( (result)=>{
+                  return {
+                    name: result.name,
+                    value: result.id
+                  }
+                })
+
+                inquirer.prompt([
+                  {
+                    type: "input",
+                    name: "roleName",
+                    message: "What is the name of the new role you would like to add?",
+                  },
+                  {
+                    type: "input",
+                    name: "salary",
+                    message: "What is the salary for this position?",
+                  },
+                  {
+                    type: "list",
+                    name: "departmentId",
+                    message: "Which department does the role belong to?",
+                    choices: departmentChoices
+                   
+                  },
+                ]).then((answers) => {
+                  db.query(
+                    "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)",
+                    [answers.roleName, parseInt (answers.salary), answers.departmentId],
+                    (err, res) => {
+                      if (err) throw err;
+                      console.log("The new role and information has been successfully to the database");
+                      mainMenu();
+
+                    }
+            
+                  );
+                  
+                });
+                
+              })
+             
+           
+            }
+
+
+
+
+
+
+
+
+
+
+
 
         function addAnEmployee() {
-          db.query(
-            'SELECT * FROM `employee`',
-            function(err, results, fields) {
-              console.log(results); 
-              mainMenu();
-            }
-          )};
-    
+          inquirer.prompt([
+             {
+               type: 'input',
+               message: 'What is the name of the first name of the employee you would like to add?',
+               name: 'adding',
+          
+             },
+             {
+              type: 'input',
+              message: 'What is the name of the surname of the employee you would like to add?',
+              name: 'adding',
+         
+            },
+            {
+              type: 'input',
+              message: 'What is the salary of the role id of the employee you would like to add?',
+              name: 'adding',
+         
+            },
+
+           ]).then(function(answer){
+             db.query(
+               "INSERT INTO employee(name)",
+               [answer.department],
+               function(err, results, fields) {
+               console.log("New employee and information added"); 
+               
+                   mainMenu();
+                 }
+                 )
+              })};
+     
+        
+        
 
          
          
