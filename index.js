bc7ffa1be923835a3d3feae5ac2fe2bb31ad2ace
Sanyiah Zulfiqar <sanyiahzulfiqar1996@gmail.com>
2022-05-21 16:43:36 +0100

    editing index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const Connection = require('mysql2/typings/mysql/lib/Connection');
 const db = require('./config/connection')
 require ('console.table')
 
@@ -51,114 +52,114 @@ mainMenu()
 
 
 
-function viewAllDepartments (){
-db.query(
-  'SELECT * FROM `department`',
-  (err, results) => {
-    if (err) {
-      console.log(err)
-    }
-    console.table(results); 
-    mainMenu();
-  }
-)};
+// function viewAllDepartments (){
+// db.query(
+//   'SELECT * FROM `department`',
+//   (err, results) => {
+//     if (err) {
+//       console.log(err)
+//     }
+//     console.table(results); 
+//     mainMenu();
+//   }
+// )};
 
 
-function viewAllRoles (){
-  db.query(
-    'SELECT * FROM `role`',
-    (err, results) =>{
-      if(err) throw err
-      console.table(results); 
-      mainMenu();
-    }
-  )};
+// function viewAllRoles (){
+//   db.query(
+//     'SELECT * FROM `role`',
+//     (err, results) =>{
+//       if(err) throw err
+//       console.table(results); 
+//       mainMenu();
+//     }
+//   )};
 
 
-  function viewAllEmployees(){
-    db.query(
-      'SELECT * FROM `employee`',
-      (err, results) =>{
-        if(err) throw err
-        console.table(results); 
-        mainMenu();
-      }
-    )};
+//   function viewAllEmployees(){
+//     db.query(
+//       'SELECT * FROM `employee`',
+//       (err, results) =>{
+//         if(err) throw err
+//         console.table(results); 
+//         mainMenu();
+//       }
+//     )};
 
 
   
-    function addADepartment() {
-     inquirer.prompt([
-        {
-          type: 'input',
-          message: 'What is the name of the new department you would like to add?',
-          name: 'adding',
+//     function addADepartment() {
+//      inquirer.prompt([
+//         {
+//           type: 'input',
+//           message: 'What is the name of the new department you would like to add?',
+//           name: 'adding',
      
-        }
-      ]).then(function(answer){
-        db.query(
-          "INSERT INTO department (name) VALUES (?)",
-          [answer.adding],
-          (err, results) =>{
-            if(err) throw err
-          console.log("New department added"); 
+//         }
+//       ]).then(function(answer){
+//         db.query(
+//           "INSERT INTO department (name) VALUES (?)",
+//           [answer.adding],
+//           (err, results) =>{
+//             if(err) throw err
+//           console.log("New department added"); 
           
-              mainMenu();
-            }
-            )
-         })};
+//               mainMenu();
+//             }
+//             )
+//          })};
 
 
     
 
-         function addARoll() {
-              db.query(
-                'SELECT * FROM `department`', (err,res)=>{
-                  if(err) throw (err)
+//          function addARoll() {
+//               db.query(
+//                 'SELECT * FROM `department`', (err,res)=>{
+//                   if(err) throw (err)
                 
-             let departmentChoices= res.map ( (result)=>{
-                  console.log (result)
-                  return {
-                    name: result.name,
-                    value: result.id
-                  }
-                })
-                console.log (departmentChoices)
+//              let departmentChoices= res.map ( (result)=>{
+//                   console.log (result)
+//                   return {
+//                     name: result.name,
+//                     value: result.id
+//                   }
+//                 })
+//                 console.log (departmentChoices)
 
-                inquirer.prompt([
-                  {
-                    type: "input",
-                    name: "roleName",
-                    message: "What is the name of the new role you would like to add?",
-                  },
-                  {
-                    type: "input",
-                    name: "salary",
-                    message: "What is the salary for this position?",
-                  },
-                  {
-                    type: "list",
-                    name: "departmentId",
-                    message: "Which department does the role belong to?",
-                    choices: departmentChoices
+//                 inquirer.prompt([
+//                   {
+//                     type: "input",
+//                     name: "roleName",
+//                     message: "What is the name of the new role you would like to add?",
+//                   },
+//                   {
+//                     type: "input",
+//                     name: "salary",
+//                     message: "What is the salary for this position?",
+//                   },
+//                   {
+//                     type: "list",
+//                     name: "departmentId",
+//                     message: "Which department does the role belong to?",
+//                     choices: departmentChoices
                    
-                  },
-                ]).then((answers) => {
+//                   },
+//                 ]).then((answers) => {
                  
-                  db.query(
-                    "INSERT INTO role SET ?",
-                    {title:answers.roleName, salary:answers.salary, department_id:parseInt(answers.departmentId)},
-                    (err, res) => {
-                      if (err) throw err;
-                      console.log("The new role and information has been successfully to the database");
-                      mainMenu()
-                    }
-                  );
-                });
+//                   db.query(
+//                     "INSERT INTO role SET ?",
+//                     {title:answers.roleName, salary:answers.salary, department_id:parseInt(answers.departmentId)},
+//                     (err, res) => {
+//                       if (err) throw err;
+//                       console.log("The new role and information has been successfully to the database");
+//                       mainMenu()
+//                     }
+//                   );
+//                 });
 
-              })
+//               })
             
-            }
+//             }
 
 
 
@@ -166,65 +167,140 @@ function viewAllRoles (){
 
 
 
-          function addAnEmployee () {
-            db.promise().query(
-              'SELECT * FROM `role`'
-            ).then ((role)=>{
-            let roleSelection= role.map ((result)=>{
-                console.log (result)
-                return {
-                  name: result.title,
-                  value: result.id
-                }
-              })
-              console.log (roleSelection)
+//           function addAnEmployee () {
+//             db.promise().query(
+//               'SELECT * FROM `role`'
+//             ).then ((role)=>{
+//             let roleSelection= role.map ((result)=>{
+//                 return {
+//                   name: result.title,
+//                   value: result.id
+//                 }
+//               })
+              
 
-          inquirer.prompt([
-             {
-               type: 'input',
-               message: 'What is the name of the first name of the employee you would like to add?',
-               name: 'firstName',
+//           inquirer.prompt([
+//              {
+//                type: 'input',
+//                message: 'What is the name of the first name of the employee you would like to add?',
+//                name: 'firstName',
           
-             },
-             {
-              type: 'input',
-              message: 'What is the name of the surname of the employee you would like to add?',
-              name: 'surname',
+//              },
+//              {
+//               type: 'input',
+//               message: 'What is the name of the surname of the employee you would like to add?',
+//               name: 'surname',
          
-            },
-            {
-              type: 'list',
-              message: 'What is the role id of the employee you would like to add?',
-              name: 'roleChoice',
-              choices: roleSelection
+//             },
+//             {
+//               type: 'list',
+//               message: 'What is the role id of the employee you would like to add?',
+//               name: 'roleChoice',
+//               choices: roleSelection
          
-            },
+//             },
 
-          ]).then(( answers ) => {
-            db.query('insert into employee SET ?', {
-              first_name: answers.firstName, 
-              last_name: answers.surname,
-              role_id: answers.roleChoice
-            }, (err,res) => {
-              if (err) throw err;
-              console.log ('The employee has been successfully added to the database')
-              mainMenu()
-            } )
-          })
+//           ]).then(( answers ) => {
+//             db.query('insert into employee SET ?', {
+//               first_name: answers.firstName, 
+//               last_name: answers.surname,
+//               role_id: answers.roleChoice
+//             }, (err,res) => {
+//               if (err) throw err;
+//               console.log ('The employee has been successfully added to the database')
+//               mainMenu()
+//             } )
+//           })
           
-        })
-        } 
+//         })
+//         } 
 
 
-  // function  updateEmployeeRole(){
-    //   db.query(
-    //     'SELECT * FROM `employee`',
-    //     (err, results) =>{
-    //       console.log(results); 
-    //       mainMenu();
-    //     }
-    //   )};
+//   function  updateEmployeeRole(){
+//    connection.query('SELECT * FROM employee',
+//    function (err, results){
+//       if(err) throw err;
+//       inquirer.prompt([
+//         {
+//           name:'choice',
+//           type:'rawlist',
+//           choices:function(){
+//             let choiceArr = [];
+//             for(i=0; i< results.length; i++)
+//             {
+//               choiceArr.push(results[i].last_name);
+//             }
+//             return choiceArr;
+//           },
+//           message: 'Select the employee you would like to update'
+//         }
+//       ])
+//       .then(function(answer){
+//         const saveName = answer.choice;
 
+//         connection.query('SELECT * FROM employee',
+//         function(err, results){
+//           if(err) throw err;
+//           inquirer.prompt([
+//               {
+//                   name: 'role',
+//                   type: 'rawlist',
+//                   choices: function(){
+//                     var choiceArr = [];
+//                     for(i=0; i< results.length; i++){
+//                       choiceArr.push(results[i].role_id)
+//                     }
+//                     return choiceArr;
+//                   },
+//                   message: 'Select the employee you would like to update'
+//               },
+//               {
+//                   name:'manager',
+//                   type: 'number',
+//                   validate: function(value){
+//                     if(isNaN(value) === false){
+//                       return true;
+//                     }
+//                     return false;
+//                   },
+//                   message: 'Please enter the new manager ID',
+//                   default: '1'
+//               }
+//              ]).then(function(answer){
+//               console.log(answer);
+//               console.log(saveName);
+//               connection.query('UPDATE employee SET ? WHERE last_name = ?',
+//               [
+//                 {
+//                   role_id: answer.role,
+//                   manager_id: answer.manager
+//                 }, saveName
+//               ],
+//               ),
+
+//               console.log('.........................');
+//               console.log('Employee updated');
+//               console.log('.........................');
+//               mainMenu;
+
+//              });
+//         })
+//       })
+//     })
+//   }
+          
+      
+      
+
+          
+          
+          
+         
+
+
+
+      
+ 
 
         
 
@@ -258,27 +334,27 @@ function viewAllRoles (){
 
 
 
-const inquirerAsync = async (questions) => {
-  try {
-    const answers = await inquirer.prompt(questions);
-    return answers;
-  } catch (error) {
-    console.log(`Error: ${error.message}`);
-    process.exit(0);
-  }
-};
+// const inquirerAsync = async (questions) => {
+//   try {
+//     const answers = await inquirer.prompt(questions);
+//     return answers;
+//   } catch (error) {
+//     console.log(`Error: ${error.message}`);
+//     process.exit(0);
+//   }
+// };
 
-const writeToFile = (path, data, options = 'utf8') => {
-  try {
-    fs.writeFileSync(path, data, options);
-    console.log(`Successfully written to ${path}!`);
-  } catch (error) {
-    console.log(`Failed to write to ${path}: ${error.message}`);
-  }
-};
+// const writeToFile = (path, data, options = 'utf8') => {
+//   try {
+//     fs.writeFileSync(path, data, options);
+//     console.log(`Successfully written to ${path}!`);
+//   } catch (error) {
+//     console.log(`Failed to write to ${path}: ${error.message}`);
+//   }
+// };
 
-const start = async () => {
-  const answers = await inquirerAsync(questions);
+// const start = async () => {
+//   const answers = await inquirerAsync(questions);
 
-  writeToFile('output.json', JSON.stringify(answers));
-};
+//   writeToFile('output.json', JSON.stringify(answers));
+// };
