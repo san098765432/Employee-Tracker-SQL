ced100e37d16f9e2d6f1b734bf0c9ea6e4a5d4a8
Sanyiah Zulfiqar <sanyiahzulfiqar1996@gmail.com>
2022-05-21 11:15:26 +0100

    editing index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function mainMenu () {
   name: 'option',
   choices: ['View all departments', 'view all roles', 'View all employees', 'Update an employee role', 'Add a department', 'Add a roll', 'Add an employee', 'Quit'], 
  }
-]).then(answers => {
+]).then((answers) => {
 switch(answers.option){
  case 'View all departments': 
  viewAllDepartments()
@@ -46,7 +46,7 @@ switch(answers.option){
 default:
   db.end()
 }
-});
+})
 } 
 mainMenu()
 
@@ -55,114 +55,115 @@ mainMenu()
 function viewAllDepartments (){
 db.query(
   'SELECT * FROM `department`',
-  function(err, results) {
-    console.log(results); 
+  (err, results) => {
+    if (err) {
+      console.log(err)
+    }
+    console.table(results); 
     mainMenu();
   }
 )};
 
 
-function viewAllRoles (){
-  db.query(
-    'SELECT * FROM `role`',
-    function(err, results, fields) {
-      console.log(results); 
-      mainMenu();
-    }
-  )};
+// function viewAllRoles (){
+//   db.query(
+//     'SELECT * FROM `role`',
+//     function(err, results) {
+//       console.table(results); 
+//       mainMenu();
+//     }
+//   )};
 
 
-  function viewAllEmployees(){
-    db.query(
-      'SELECT * FROM `employee`',
-      function(err, results, fields) {
-        console.log(results); 
-        mainMenu();
-      }
-    )};
+//   function viewAllEmployees(){
+//     db.query(
+//       'SELECT * FROM `employee`',
+//       function(err, results) {
+//         console.table(results); 
+//         mainMenu();
+//       }
+//     )};
 
 
-    function  updateEmployeeRole(){
-      db.query(
-        'SELECT * FROM `employee`',
-        function(err, results, fields) {
-          console.log(results); 
-          mainMenu();
-        }
-      )};
+//     function  updateEmployeeRole(){
+//       db.query(
+//         'SELECT * FROM `employee`',
+//         function(err, results, fields) {
+//           console.log(results); 
+//           mainMenu();
+//         }
+//       )};
 
 
-    function addADepartment() {
-     inquirer.prompt([
-        {
-          type: 'input',
-          message: 'What is the name of the new department you would like to add?',
-          name: 'adding',
+//     function addADepartment() {
+//      inquirer.prompt([
+//         {
+//           type: 'input',
+//           message: 'What is the name of the new department you would like to add?',
+//           name: 'adding',
      
-        }
-      ]).then(function(answer){
-        db.query(
-          "INSERT INTO department (name) VALUES (?)",
-          [answer],
-          function(err, results, fields) {
-          console.log("New department added"); 
+//         }
+//       ]).then(function(answer){
+//         db.query(
+//           "INSERT INTO department (name) VALUES (?)",
+//           [answer],
+//           function(err, results, fields) {
+//           console.log("New department added"); 
           
-              mainMenu();
-            }
-            )
-         })};
+//               mainMenu();
+//             }
+//             )
+//          })};
 
 
     
 
-          async function addARoll(departmentID, departmentName) {
-              db.promise().query(
-                'SELECT * FROM `department`'
-              ).then (([departments])=>{
-                departmentChoices= departments.map ( (result)=>{
-                  return {
-                    name: result.name,
-                    value: result.id
-                  }
-                })
+//           async function addARoll() {
+//               db.promise().query(
+//                 'SELECT * FROM `department`'
+//               ).then ((departments)=>{
+//                departmentChoices= departments.map ( (result)=>{
+//                   console.log (result)
+//                   return {
+//                     name: result.name,
+//                     value: result.id
+//                   }
+//                 })
+//                 console.log (departmentChoices)
 
-                inquirer.prompt([
-                  {
-                    type: "input",
-                    name: "roleName",
-                    message: "What is the name of the new role you would like to add?",
-                  },
-                  {
-                    type: "input",
-                    name: "salary",
-                    message: "What is the salary for this position?",
-                  },
-                  {
-                    type: "list",
-                    name: "departmentId",
-                    message: "Which department does the role belong to?",
-                    choices: departmentChoices
+//                 inquirer.prompt([
+//                   {
+//                     type: "input",
+//                     name: "roleName",
+//                     message: "What is the name of the new role you would like to add?",
+//                   },
+//                   {
+//                     type: "input",
+//                     name: "salary",
+//                     message: "What is the salary for this position?",
+//                   },
+//                   {
+//                     type: "list",
+//                     name: "departmentId",
+//                     message: "Which department does the role belong to?",
+//                     choices: departmentChoices
                    
-                  },
-                ]).then((answers) => {
-                  db.query(
-                    "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)",
-                    [answers.roleName, parseInt (answers.salary), answers.departmentId],
-                    (err, res) => {
-                      if (err) throw err;
-                      console.log("The new role and information has been successfully to the database");
-                      mainMenu();
+//                   },
+//                 ]).then((answers) => {
+                 
+//                   db.query(
+//                     "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)",
+//                     [answers.roleName, answers.salary, parseInt(answers.departmentId)],
+//                     (err, res) => {
+//                       if (err) throw err;
+//                       console.log("The new role and information has been successfully to the database");
+//                     }
+//                   );
+//                 });
 
-                    }
+//               })
             
-                  );
-                  
-                });
-                
-              })
-             
-           
-            }
+//             }
 
 
 
@@ -170,46 +171,57 @@ function viewAllRoles (){
 
 
 
+//           function addAnEmployee () {
+//             db.promise().query(
+//               'SELECT * FROM `role`'
+//             ).then ((role)=>{
+//             let roleSelection= role.map ((result)=>{
+//                 console.log (result)
+//                 return {
+//                   name: result.title,
+//                   value: result.id
+//                 }
+//               })
+//               console.log (roleSelection)
 
-
-
-
-
-        function addAnEmployee() {
-          inquirer.prompt([
-             {
-               type: 'input',
-               message: 'What is the name of the first name of the employee you would like to add?',
-               name: 'adding',
+//           inquirer.prompt([
+//              {
+//                type: 'input',
+//                message: 'What is the name of the first name of the employee you would like to add?',
+//                name: 'firstName',
           
-             },
-             {
-              type: 'input',
-              message: 'What is the name of the surname of the employee you would like to add?',
-              name: 'adding',
+//              },
+//              {
+//               type: 'input',
+//               message: 'What is the name of the surname of the employee you would like to add?',
+//               name: 'surname',
          
-            },
-            {
-              type: 'input',
-              message: 'What is the salary of the role id of the employee you would like to add?',
-              name: 'adding',
+//             },
+//             {
+//               type: 'list',
+//               message: 'What is the role id of the employee you would like to add?',
+//               name: 'roleChoice',
+//               choices: roleSelection
          
-            },
+//             },
 
-           ]).then(function(answer){
-             db.query(
-               "INSERT INTO employee(name)",
-               [answer.department],
-               function(err, results, fields) {
-               console.log("New employee and information added"); 
-               
-                   mainMenu();
-                 }
-                 )
-              })};
-     
+//           ]).then(( answers ) => {
+//             db.query('insert into employee SET', {
+//               first_name: answers.firstName, 
+//               last_name: answers.surname,
+//               role_id: answers.roleChoice
+//             }, (err,res) => {
+//               if (err) throw err;
+//               console.log ('The employee has been successfully added to the database')
+//             } )
+//           })
+          
+//         })
+//         } 
+
+
+
         
-        
 
          
          
@@ -241,27 +253,27 @@ function viewAllRoles (){
 
 
 
-const inquirerAsync = async (questions) => {
-  try {
-    const answers = await inquirer.prompt(questions);
-    return answers;
-  } catch (error) {
-    console.log(`Error: ${error.message}`);
-    process.exit(0);
-  }
-};
+// const inquirerAsync = async (questions) => {
+//   try {
+//     const answers = await inquirer.prompt(questions);
+//     return answers;
+//   } catch (error) {
+//     console.log(`Error: ${error.message}`);
+//     process.exit(0);
+//   }
+// };
 
-const writeToFile = (path, data, options = 'utf8') => {
-  try {
-    fs.writeFileSync(path, data, options);
-    console.log(`Successfully written to ${path}!`);
-  } catch (error) {
-    console.log(`Failed to write to ${path}: ${error.message}`);
-  }
-};
+// const writeToFile = (path, data, options = 'utf8') => {
+//   try {
+//     fs.writeFileSync(path, data, options);
+//     console.log(`Successfully written to ${path}!`);
+//   } catch (error) {
+//     console.log(`Failed to write to ${path}: ${error.message}`);
+//   }
+// };
 
-const start = async () => {
-  const answers = await inquirerAsync(questions);
+// const start = async () => {
+//   const answers = await inquirerAsync(questions);
 
-  writeToFile('output.json', JSON.stringify(answers));
-};
+//   writeToFile('output.json', JSON.stringify(answers));
+// };
